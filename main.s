16ae59e4c77ee1146ca1d8deef78b88f91702d7f
faizal <feglenn1@cougars.ccis.edu>
2015-11-01 22:14:44 -0600

    ask user for row and col write function to convert row and  col to postions on the board

--- main.s
+++ main.s
@@ -5,19 +5,65 @@
 # Description:
 # This is a simple tic-tac-toe game with a basic AI.
 #
-# Register usage:
+# Register usage: $s0 = input_row
+#                  $s1 = input_col
 #
 # ============================================================================
 
           .text
           .globl    main
 
-main: li $v0, 5 # get input 
-	
+main:
+GAMELOOP:    
+                  li    $v0, 4    
+                la   $a0, INPUTROW
+                syscall 
+
+li    $v0, 5 #get row selection from user
+               syscall
+               move $s0, $v0
+
+            li    $v0, 4    
+                   la   $a0, INPUTCOLUMN
+                   syscall 
+        
+            li    $v0,5 #get column selection from user
+               syscall
+               move $s1, $v0
+
+            j GAMELOOP
 
 EXIT:
-          li        $v0, 10
-          syscall
+              li        $v0, 10
+              syscall
+# Converts a row and column to an index for a 1-D array.
+# INPUTS:
+# a0 = row
+# a1 = col
+# OUTPUTS:
+# v0 = index
+ROWCOLTOINDEX: 
+        move $v0, $a0
+        li   $t0, 3
+        mul  $v0, $v0, $t0
+        add  $v0, $v0, $a1
+        
+        jr   $ra
 
+# -------
+# | | | |
+# |-----|
+# | |X| |
+# |-----|
+# | | |O|
+# -------
 
-          .data
+PRINTBOARD:
+
+              .data
+INPUTROW:         .asciiz "Enter row: "
+INPUTCOLUMN:     .asciiz "Enter column: "
+BOARD:
+        .byte 32 32 32
+        .byte 32 32 32
+        .byte 32 32 32
